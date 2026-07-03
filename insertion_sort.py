91adaa2ca062ad50ca0d5d8e30410b2fd5f6ca74
AdityaRaya <aditya.rgs123@gmail.com>
2026-07-03 18:38:02 -0500

    Implement insertion sort in decreasing order

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,4 +5,14 @@ in monotonically decreasing order.
 """
 
 def insertion_sort_desc(arr):
-    pass
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
